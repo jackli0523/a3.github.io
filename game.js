8b8302f0ba3b2fe0bf6952b89e96294540963247
jackli0523 <132292582+jackli0523@users.noreply.github.com>
2023-06-02 14:44:42 +0800

    add logo class

--- game.js
+++ game.js
@@ -1,1 +1,48 @@
+class Logo extends Phaser.Scene {
+    constructor() {
+        super('logo')
+    }
+    preload() {
+        // load path use assets/
+        this.load.path = "./assets/";
+        this.load.image("logo", "logo.png");
+    }
+    create() {
+        // create method
+        // set background color
+        this.cameras.main.setBackgroundColor(0x000000);
+        
+        // animation effects
+        this.cameras.main.fadeIn(500, 0, 0, 0);
 
+        let logo = this.add.image(
+            game.canvas.width / 2, -300,
+            "logo"
+        );
+
+        this.tweens.add({
+            targets: logo,
+            y: game.canvas.height / 2 - 50,
+            alpha: 1,
+            delay: 1000,
+            duration: 2000,
+            ease: "Linear",
+            repeat: 0
+        });
+
+        this.tweens.add({
+            targets: logo,
+            y: game.canvas.height + 300,
+            alpha: 1,
+            delay: 5000,
+            duration: 2000,
+            ease: "Linear",
+            repeat: 0,
+            onComplete: () => this.scene.start('rule')
+        });
+        // setTimeout(() => {
+        //     this.cameras.main.fade(500, 0, 0, 0);
+        //     this.time.delayedCall(500, () => 
+        // }, 7000);
+    }
+}
